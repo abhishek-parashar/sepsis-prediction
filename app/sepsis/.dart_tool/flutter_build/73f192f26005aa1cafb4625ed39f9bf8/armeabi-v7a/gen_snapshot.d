9f91775102f41172e4d136148ffffd89325bdd2c
Ankit Vashisht <ankitvashisht12@gmail.com>
2020-01-12 21:01:03 +0530

    Flutter app - sepsis

---
+++ app/sepsis/.dart_tool/flutter_build/73f192f26005aa1cafb4625ed39f9bf8/armeabi-v7a/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /home/av/Program_Files/flutter/bin/cache/artifacts/engine/android-arm-release/linux-x64/gen_snapshot
---
+++ app/sepsis/.dart_tool/flutter_build/73f192f26005aa1cafb4625ed39f9bf8/x86_64/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /home/av/Program_Files/flutter/bin/cache/artifacts/engine/android-x64-release/linux-x64/gen_snapshot
